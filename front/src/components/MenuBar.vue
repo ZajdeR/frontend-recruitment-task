1622b1c712fff88e8122455f48e215fe3b33d9d5
Pawel <pawelzajder@gmail.com>
2020-07-21 00:40:57 +0200

    MenuBar.vue - change styling

--- front/src/components/MenuBar.vue
+++ front/src/components/MenuBar.vue
@@ -1,22 +1,18 @@
 <template>
   <v-app-bar app color="primary" dark>
-    <div class="col-md-9">
-      <v-btn class="mx-2" @click.prevent="$router.push('/')">
-        {{ $t('MENU.BUTTON.0') }}
-      </v-btn>
-      <v-btn v-for="menuItem in menuItems" :key="menuItem.id" :class="['mx-2', menuItem.class]" @click.prevent="doRedirect(`/task/${menuItem.id}`)">
-        {{ $t(`MENU.BUTTON.${menuItem.id}`) }}
-      </v-btn>
-    </div>
-
-    <div class="col-md-3 d-flex justify-end">
-      <v-btn text @click="$root.$i18n.locale = 'pl'">
-        <v-img src="../assets/pl.svg" max-width="30" class="rounded-circle" />
-      </v-btn>
-      <v-btn text @click="$root.$i18n.locale = 'en'">
-        <v-img src="../assets/en.svg" max-width="30" class="rounded-circle" />
-      </v-btn>
-    </div>
+    <v-btn class="mx-2" @click.prevent="$router.push('/')">
+      {{ $t('MENU.BUTTON.0') }}
+    </v-btn>
+    <v-btn v-for="menuItem in menuItems" :key="menuItem.id" :class="['mx-2', menuItem.class]" @click.prevent="doRedirect(`/task/${menuItem.id}`)">
+      {{ $t(`MENU.BUTTON.${menuItem.id}`) }}
+    </v-btn>
+    <v-spacer />
+    <v-btn text @click="$root.$i18n.locale = 'pl'">
+      <v-img src="../assets/pl.svg" max-width="30" class="rounded-circle" />
+    </v-btn>
+    <v-btn text @click="$root.$i18n.locale = 'en'">
+      <v-img src="../assets/en.svg" max-width="30" class="rounded-circle" />
+    </v-btn>
   </v-app-bar>
 </template>
 
