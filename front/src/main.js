f344be2f6590854233d29f0d02c11ae4bfac1c79
Pawel <pawelzajder@gmail.com>
2020-07-20 22:57:39 +0200

    configure plugins (i18n, axios, toasted)

--- front/src/main.js
+++ front/src/main.js
@@ -3,12 +3,22 @@ import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
 import vuetify from '@/plugins/vuetify';
+import i18n from '@/plugins/i18n';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import Toasted from 'vue-toasted';
 
 Vue.config.productionTip = false;
+
+Vue.use(Toasted);
+Vue.use(VueAxios, axios);
+
+axios.defaults.baseURL = process.env.API_URL;
 
 new Vue({
   router,
   store,
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
